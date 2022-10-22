be6e67004f5a0e322ad2288e1462a471be8aba0c
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-10-22 11:11:42 +0200

    Create combustion.bash

---
+++ combustion.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo 'root:HASHchangeME' | chpasswd -e
+
+zypper --non-interactive install podman docker-compose
+systemctl disable docker
+systemctl enable podman
+systemctl enable sshd.service
+
+mkdir -p /var/orchestra
+cd /var/orchestra
+wget https://raw.githubusercontent.com/HPPinata/Xen-Orchestra-Install/compose.yml
+
+cat <<'EOL' > /var/orchestra/update.bash
+#!/bin/bash
+docker-compose down
+docker-compose pull
+docker-compose build --pull
+docker-compose up -dV
+EOL
+chmod +x /var/orchestra/update.bash
+
+cat <<'EOL' > /etc/systemd/system/orchestra-compose.service
+[Unit]
+Description=Start Xen-Orchestra Container
+After=network-online.target
+
+[Service]
+Type=oneshot
+ExecStart=/var/orchestra/update.bash
+ExecStop=/bin/docker-compose down -f /var/orchestra/compose.yml
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
