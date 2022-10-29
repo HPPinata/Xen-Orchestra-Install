b85f85e9a7481c0d35b29e59d4d939c6d0cd39e9
HPPinata <83947761+HPPinata@users.noreply.github.com>
2022-10-29 14:41:24 +0200

    change order

--- combustion.bash
+++ combustion.bash
@@ -7,17 +7,6 @@ systemctl enable docker
 
 mount /dev/xvdb4 /var
 
-mount /dev/sr1 /mnt
-zypper rm -yu xen-tools-domU
-zypper in -y --allow-unsigned-rpm /mnt/Linux/*.x86_64.rpm
-
-cp -f /mnt/Linux/xen-vcpu-hotplug.rules /etc/udev/rules.d/
-cp -f /mnt/Linux/xe-linux-distribution.service /etc/systemd/system/
-sed -i 's+share/oem/xs+sbin+g' /etc/systemd/system/xe-linux-distribution.service
-sed -i 's+ /var/cache/xe-linux-distribution++g' /etc/systemd/system/xe-linux-distribution.service
-systemctl daemon-reload
-systemctl enable /etc/systemd/system/xe-linux-distribution
-
 mkdir -p /var/orchestra
 cd /var/orchestra
 wget https://raw.githubusercontent.com/HPPinata/Xen-Orchestra-Install/main/compose.yml
@@ -46,3 +35,14 @@ RemainAfterExit=yes
 WantedBy=multi-user.target
 EOL
 systemctl enable orchestra-compose
+
+mount /dev/sr1 /mnt
+zypper rm -yu xen-tools-domU
+zypper in -y --allow-unsigned-rpm /mnt/Linux/*.x86_64.rpm
+
+cp -f /mnt/Linux/xen-vcpu-hotplug.rules /etc/udev/rules.d/
+cp -f /mnt/Linux/xe-linux-distribution.service /etc/systemd/system/
+sed -i 's+share/oem/xs+sbin+g' /etc/systemd/system/xe-linux-distribution.service
+sed -i 's+ /var/cache/xe-linux-distribution++g' /etc/systemd/system/xe-linux-distribution.service
+systemctl daemon-reload
+systemctl enable /etc/systemd/system/xe-linux-distribution
