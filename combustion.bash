f2630a90530fd9ea4d7a9655c9d60fa68c57d686
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-10-22 22:55:43 +0200

    no systemd

--- combustion.bash
+++ combustion.bash
@@ -14,7 +14,7 @@ mount /dev/sr1 /mnt
 zypper rm -yu xen-tools-domU
 /mnt/Linux/install.sh -d sles -m 15 -n
 
-zypper in -y docker-compose curl zram-generator
+zypper in -y cron curl docker zram-generator
 systemctl enable docker
 
 cat <<'EOL' > /etc/systemd/zram-generator.conf
@@ -33,25 +33,15 @@ curl -O https://raw.githubusercontent.com/HPPinata/Xen-Orchestra-Install/main/co
 cat <<'EOL' > /var/orchestra/update.bash
 #!/bin/bash
 cd /var/orchestra
-docker-compose pull
-docker-compose build --pull
-docker-compose up -dV
+docker compose pull
+docker compose build --pull
+docker compose up -dV
 docker system prune -a -f --volumes
 EOL
 chmod +x /var/orchestra/update.bash
 
-cat <<'EOL' > /etc/systemd/system/orchestra-compose.service
-[Unit]
-Description=Start Xen-Orchestra Container
-After=network-online.target docker.service
-
-[Service]
-Type=oneshot
-ExecStart=bash -c '/var/orchestra/update.bash'
-ExecStop=bash -c '/bin/docker-compose down -f /var/orchestra/compose.yml'
-RemainAfterExit=yes
-
-[Install]
-WantedBy=multi-user.target
+cat <<'EOL' | crontab -
+SHELL=/bin/bash
+BASH_ENV=/etc/profile
+@reboot /var/orchestra/update.bash
 EOL
-systemctl enable /etc/systemd/system/orchestra-compose.service
